acf128ecb4455f5468a4eb06c7f2379432128b4f
Joey Biotti <joey@MacBook-Pro-2.local>
2017-12-16 21:12:04 -0600

    refactored program

--- bday_json/bdays_json.py
+++ bday_json/bdays_json.py
@@ -1,39 +1,14 @@
 import json
 
-answer = 'yes'
+birthday = {}
+with open('birthdays.json') as f:
+    birthday = json.load(f)
 
-while answer != 'no':
-    with open ('birthdays.json', 'r') as f:
-        birthday =  json.load(f)
+def add_bday():
+    name = input('Who do you want to add to the dictionary?\n').title()
+    date = input('When was {} born?\n.'.format(name))
+    birthday[name] = date
+    with open('birthdays.json', 'w') as f:
+        json.dump(birthday, f)
+    print('{} added to the list\n'.format(name))
 
-        print('Here\'s the people that are in the Birthday Dictionary...')
-        for i in birthday:
-            print ("\n" + i)
-
-        ans = input("1: Find someone's birthday. \n OR! \n 2: Add a new person's birthday. \n")
-
-        if ans == "1":
-            name = input("Enter the person's name: ")
-            print ("The birthday for {} is {}".format(name, data.get(name, "Ain't in the database.")))
-
-        elif ans == "2":
-            n = int(input("How many birthdays you wanna add?"))
-            i = 0
-            while i < n:
-                print("\nName", i+1)
-                new_name = input("\nEnter name: ")
-                new_bd = input("\nEnter their birthday (mm/dd/yyyy): ")
-
-                data[new_name] = new_bd
-
-                with open('birthdays.json', 'w') as f:
-                    json.dump(data, f)
-
-                print ('Birthday added! ')
-
-                i += 1
-
-            else:
-                print('\nInvalid choice. DO IT AGAIN! ')
-
-            answer = input('Do you want to add more people?')
